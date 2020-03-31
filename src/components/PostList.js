00258a5fb197b1ae70a610c6b3442a9116cfaac3
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-03-31 19:51:33 +0300

    Initial commit

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PostList extends React.Component {
+    componentDidMount = () => {
+
+    }
+    
+    render(){
+        return <div>Post List</div>
+    }
+}
+
+export default PostList;
